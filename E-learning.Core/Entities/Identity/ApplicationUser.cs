438dcac0f7dd0bf9dceb89c72658a2c49d7732c2
Aya Samir <ayasamir1810@gmail.com>
2026-03-04 17:17:02 +0200

    add Applicationuser

---
+++ E-learning.Core/Entities/Identity/ApplicationUser.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_learning.Core.Entities.Identity
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public string FullName { get; set; } = string.Empty;
+        public string Bio { get; set; } = string.Empty;
+        public string ProfileImage { get; set; } = string.Empty;
+        public string Location { get; set; } = string.Empty;
+        public DateOnly DateOfBirth { get; set; }
+        //public Status IsActive { get; set; }
+        public DateTime MemberSince { get; set; }
+        public DateTime UpdatedAt { get; set; }
+    }
+}
