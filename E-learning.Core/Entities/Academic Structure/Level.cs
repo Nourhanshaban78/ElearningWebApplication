343936a8dd51b55fc2049c271dd4df96e6396888
menna-magdy0-0 <mennamagdy4291@gmail.com>
2026-03-04 22:12:19 +0200

    stages and levels entity

---
+++ E-learning.Core/Entities/Academic Structure/Level.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_learning.Core.Entities.Academic_Structure
+{
+    public class Level
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        [Required]
+        [ForeignKey("Stage")]
+        public Guid StageId { get; set; }
+
+        public Stage? Stage { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string Name { get; set; } = string.Empty;
+
+        [Required]
+        public int OrderIndex { get; set; }
+
+        public bool IsActive { get; set; }=true;
+
+        public DateTime CreatedAt { get; set; }
+
+        [NotMapped]
+        public int CourseCount { get; set; }
+
+
+    }
+}
