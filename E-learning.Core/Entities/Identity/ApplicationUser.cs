56499b81bb18cc5241eff2197a17ec189c4b0009
Aya Samir <ayasamir1810@gmail.com>
2026-03-04 21:25:11 +0200

    Add BaseEntity , Enrollment and Learning progress

--- E-learning.Core/Entities/Identity/ApplicationUser.cs
+++ E-learning.Core/Entities/Identity/ApplicationUser.cs
@@ -1,21 +1,27 @@
 ﻿using Microsoft.AspNetCore.Identity;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace E_learning.Core.Entities.Identity
 {
     public class ApplicationUser : IdentityUser
     {
         public string FullName { get; set; } = string.Empty;
-        public string Bio { get; set; } = string.Empty;
-        public string ProfileImage { get; set; } = string.Empty;
-        public string Location { get; set; } = string.Empty;
-        public DateOnly DateOfBirth { get; set; }
-        //public Status IsActive { get; set; }
-        public DateTime MemberSince { get; set; }
-        public DateTime UpdatedAt { get; set; }
+        public string? Bio { get; set; }
+        public string? ProfileImage { get; set; }
+        public string? Location { get; set; }
+        public DateOnly? DateOfBirth { get; set; }
+        public bool IsActive { get; set; } = true;
+        public DateTime MemberSince { get; set; } = DateTime.UtcNow;
+        public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
+
+        //// Preferences
+        //public string Language { get; set; } = "en";
+        //public string TimeZone { get; set; } = "UTC";
+        //public bool ProfileVisibility { get; set; } = true;
+        //public bool ShowProgressToOthers { get; set; } = true;
+
+        //// Notifications
+        //public bool NotifyInApp { get; set; } = true;
+        //public bool NotifyEmail { get; set; } = true;
     }
 }
