3f393c62e9682f2d5c5dc2cb6e719e153c68d954
Abdullah Mustafa <abdullah.sci02@gmail.com>
2026-03-13 07:41:17 +0200

    Add INotification and INotificaionsSetting

--- E-Learning.Core/Interfaces/Repositories/Notifications/INotificationRepository.cs
+++ E-Learning.Core/Interfaces/Repositories/Notifications/INotificationRepository.cs
@@ -1,6 +1,17 @@
-﻿namespace E_Learning.Core.Interfaces.Repositories.Notifications
+﻿using E_Learning.Core.Entities.Notifications;
+using E_Learning.Core.Enums;
+
+namespace E_Learning.Core.Interfaces.Repositories.Notifications
 {
-    public interface INotificationRepository
+    public interface INotificationRepository : IGenericRepository<Notification, Guid> 
     {
+        Task<(IReadOnlyList<Notification> Items, int TotalCount)> GetUserNotificationsPagedAsync(
+            Guid userId, int page, int pageSize, bool? isRead, NotificationType? type);
+
+        Task<int> GetUnreadCountAsync(Guid userId);
+
+        Task<Notification?> GetByIdForUserAsync(Guid notificationId, Guid userId);
+
+        Task<int> MarkAllAsReadAsync(Guid userId);
     }
 }
