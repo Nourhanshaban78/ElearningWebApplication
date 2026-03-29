e12b8f2a4baa323b002b768b53262a20e08b44a6
Ahmed Atef <ahmed.shera10@gmail.com>
2026-03-29 18:56:46 +0200

    end point for admin dashboad

---
+++ E-Learning.API/Controllers/DashboardAdminController.cs
@@ -0,0 +1,31 @@
+﻿using E_Learning.Service.Services.AdminDashboard;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace E_Learning.API.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DashboardAdminController : ControllerBase
+    {
+        private readonly IAdminDashboardService _dashboardService;
+
+        public DashboardAdminController(IAdminDashboardService dashboardService)
+        {
+            _dashboardService = dashboardService;
+        }
+
+        [HttpGet("overview")]
+        public async Task<IActionResult> GetOverview(CancellationToken ct)
+        {
+            var response = await _dashboardService.GetDashboardOverviewAsync(ct);
+ 
+            if (response.Succeeded)
+                return Ok(response);
+
+            return BadRequest(response);
+        }
+    }
+}
